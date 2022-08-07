4942729c1b2326d99a90cffe5d52d97e2daf86aa
Juan Manuel De Senzi <juanmadesenzi@gmail.com>
2022-08-07 11:27:44 +0200

    Desafío clase 7 y 8

---
+++ contenedor.js
@@ -0,0 +1,92 @@
+import * as fs from "fs";
+
+export default class Contenedor {
+  getAll() {
+    let data, file;
+    try {
+      file = fs.readFileSync("data.json", "utf-8");
+    } catch (err) {
+      console.log(
+        "Hubo un error al leer el archivo en la función getAll(): " + err
+      );
+    }
+    if (file) {
+      data = JSON.parse(file);
+    }
+    return file ? data : [];
+  }
+
+  save(producto) {
+    let listaProductos = this.getAll();
+    let listaProductosJSON;
+
+    if (listaProductos.length != 0) {
+      let indiceIDs = listaProductos.map((x) => x.id).sort();
+      //le asigno el número más alto de los ids existentes + 1
+      producto.id = indiceIDs[indiceIDs.length - 1] + 1;
+    } else {
+      //Si la lista está vacía le asigno id = 0
+      producto.id = 0;
+    }
+    //Agrego el producto a la lista
+    listaProductos.push(producto);
+    listaProductosJSON = JSON.stringify(listaProductos);
+    try {
+      fs.writeFileSync("data.json", listaProductosJSON);
+    } catch (err) {
+      console.log(
+        "Hubo un error al intentar reescribir el archivo en save(): " + err
+      );
+    }
+    return producto.id;
+  }
+
+  getById(numeroID) {
+    let listaProductos = this.getAll();
+    let producto;
+    listaProductos.forEach((element) => {
+      if (element.id == numeroID) producto = element;
+    });
+    if (producto) {
+      return producto;
+    } else {
+      console.log("No existe un producto con el número de ID: " + numeroID);
+    }
+  }
+
+  deleteById(numeroID) {
+    let listaProductos = this.getAll();
+    let indexDelProductoAEliminar;
+    listaProductos.forEach((element, ind) => {
+      if (element.id == numeroID) indexDelProductoAEliminar = ind;
+    });
+    //Chequeo que haya coincidencia de ID y que el array tenga más de un elemento para usar splice
+    if (indexDelProductoAEliminar != undefined && listaProductos.length > 1) {
+      //Creo la nueva lista sin el producto seleccionado
+      listaProductos.splice(indexDelProductoAEliminar, 1);
+      let nuevaListaProductosJSON = JSON.stringify(listaProductos);
+      try {
+        fs.writeFileSync("data.json", nuevaListaProductosJSON);
+      } catch (err) {
+        console.log(
+          "Hubo un error al intentar reescribir el archivo en deleteById(): " +
+            err
+        );
+      }
+    } else if (
+      indexDelProductoAEliminar != undefined &&
+      listaProductos.length == 1
+    ) {
+      this.deleteAll();
+    } else {
+      console.log(
+        `El producto con el ID ${numeroID} no existe en nuestra base de datos`
+      );
+    }
+    return indexDelProductoAEliminar ? true : false;
+  }
+
+  deleteAll() {
+    fs.writeFileSync("data.json", "");
+  }
+}
---
+++ main.js
@@ -0,0 +1,73 @@
+import Contenedor from "./contenedor.js";
+import express from "express";
+import path from "path";
+
+const { Router } = express;
+const app = express();
+const router = Router();
+const PORT = 8080;
+const server = app.listen(PORT);
+let productos = new Contenedor();
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/productos", router);
+app.use("public", express.static(__dirname + "/public"));
+
+//Aquí estoy seteando que cuando entren a localhost.com/ les devuelva index.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.get("/form", (request, response) => {
+  response.redirect("/");
+});
+
+router.get("/", (request, response) => {
+  const { query } = request;
+  //Chequeo si hay algo en el query
+  if (Object.keys(query).length === 0) {
+    response.json(productos.getAll());
+  }
+});
+
+router.get("/:id", (request, response) => {
+  const { id } = request.params;
+  const productList = productos.getAll();
+  const productFound = productList.find((element) => element.id == id);
+  productFound
+    ? response.json(productFound)
+    : response.json(
+        `El producto con ID ${id}, no se encuentra en nuestra lista de productos`
+      );
+});
+
+router.post("/", (request, response) => {
+  const { body } = request;
+  const productAdded = productos.save(body);
+  productAdded
+    ? response.json({
+        success: "ok",
+        newProduct: productos.getById(productAdded),
+      })
+    : response.json("Error, el producto no pudo ser agregado");
+});
+
+router.put("/:id", (request, response) => {
+  const { id } = request.params;
+  const productToModify = productos.getById(id);
+  productToModify.price += 1;
+  response.json({
+    success: "ok",
+    message: "El precio del producto fue incrementado en $1 exitosamente",
+  });
+});
+
+router.delete("/:id", (request, response) => {
+  const { id } = request.params;
+  const productDeleted = productos.deleteById(id);
+  productDeleted
+    ? response.json({ success: "ok" })
+    : response.json("El producto no se encuentra en nuestra base de datos");
+});
